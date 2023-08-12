355fff3f0bac2ae7397d92029c54ec279f52fc0d
Bolshiyanov <bolshiyanov@gmail.com>
2023-08-12 21:56:17 +0100

    Added cathing errors to start file

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,19 +4,61 @@ import { Provider } from "react-redux";
 import { setupStore } from "@/store/store";
 
 import { Drawer } from "@/src/utils/navigations/Drawer";
-import { Link } from "expo-router";
+import { SplashScreen, Link } from "expo-router";
 import { Pressable } from "react-native";
+
+import { useEffect } from 'react';
+import { useColorScheme } from 'react-native';
 
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import Counter from "@/src/utils/counter";
+
+import { useFonts } from 'expo-font';
+
+const store = setupStore();
 
 export const unstable_settings = {
   initialRouteName: "index",
 };
 
-const store = setupStore();
+export {
+  // Catch any errors thrown by the Layout component.
+  ErrorBoundary,
+} from 'expo-router';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+
 
 export default function RootLayout() {
+
+  const [loaded, error] = useFonts({
+    SpaceMono: require('@/assets/fonts/SpaceMono-Regular.ttf'),
+    ...FontAwesome.font,
+  });
+
+  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
+  useEffect(() => {
+    if (error) throw error;
+  }, [error]);
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded]);
+
+  if (!loaded) {
+    return null;
+  }
+
+  return <RootLayoutNav />;
+}
+
+function RootLayoutNav() {
+  const colorScheme = useColorScheme();
+
   return (
     <Provider store={store}>
       <Drawer>
